b24d1b999ef11f003da76a449f44aae0b7456d09
Eloi <eloirobe@gmail.com>
2015-06-14 21:28:20 +0200

    Using parse yml and parse json

--- web/index.php
+++ web/index.php
@@ -1,7 +1,21 @@
 <?php
 
 require '../vendor/autoload.php';
-use FW\Application;
+use Fw\Components\Routing\RouteParser\ymlParser;
+use Fw\Application;
+use Fw\Components\Routing\RouteParser\jsonParser;
 
 $application = new Application();
+
+$routing=new ymlParser(__DIR__."/../src/config/routing.yml");
+
+$application->setRouting($routing);
 $application->run();
+
+
+$routing=new jsonParser(__DIR__."/../src/config/routing.json");
+
+$application->setRouting($routing);
+$application->run();
+
+
